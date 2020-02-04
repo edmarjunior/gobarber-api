40e627063a9303bd2c14185b1191569d4edcc169
Edmar Junior <edmarjunior17@gmail.com>
2020-02-04 10:10:12 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => res.json({ message: 'Hello word with sucrase-nodemon' }));
+
+export default routes;
