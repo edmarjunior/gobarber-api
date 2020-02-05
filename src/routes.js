5b9970fa5f528b15d0f7d56614f2cd52daed7f0c
Edmar Junior <edmarjunior17@gmail.com>
2020-02-05 18:32:22 -0300

    Instalando e configurando eslint, prettier e editor config

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello word with sucrase-nodemon' }));
+routes.get('/', (req, res) =>
+	res.json({ message: 'Hello word with sucrase-nodemon' })
+);
 
 export default routes;
