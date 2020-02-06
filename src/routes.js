c205426a7784fa07e0c4b122428232334a922def
Edmar Junior <edmarjunior17@gmail.com>
2020-02-06 15:58:31 -0300

    Implementando os recursos de usuario e sessão; adicionando jwt, yup e middlewares

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
