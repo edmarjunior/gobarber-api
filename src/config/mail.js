65fe9416f32eab9acab16895068d4bdaa5f6935c
edmar-junior <edmar.acjr@gmail.com>
2020-03-07 14:11:05 -0300

    Enviando e-mail de cancelamento de agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	auth: {
+		user: 'e6b24873bf6390',
+		pass: 'e0f2c2266ce446',
+	},
+	secure: false,
+	default: {
+		from: 'Equipe GoBarber <noreplay@gobarber.com>',
+	},
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,24 @@
+import nodeMailer from 'nodemailer';
+import mailconfig from '../config/mail';
+
+class Mail {
+	constructor() {
+		const { host, port, secure, auth } = mailconfig;
+
+		this.transporter = nodeMailer.createTransport({
+			host,
+			port,
+			secure,
+			auth: auth.user ? auth : null,
+		});
+	}
+
+	sendMail(message) {
+		return this.transporter.sendMail({
+			...mailconfig.default,
+			...message,
+		});
+	}
+}
+
+export default new Mail();
