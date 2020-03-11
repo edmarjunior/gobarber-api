9593f074bd955833ea53ad10edfbc8817a07f10f
edmar-junior <edmar.acjr@gmail.com>
2020-03-11 00:43:41 -0300

    Criando testes de integração para cadastro de usuários

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('User', () => {
+	it('Deve ser possível realizar o cadastro', async () => {
+		const response = await request(app)
+			.post('/users')
+			.send({
+				name: 'Edmar',
+				email: 'edmar2@smn.com.br',
+				password: '123456',
+			});
+		expect(response.body).toHaveProperty('id');
+	});
+
+	it('Não deve permitir cadastrar usuário com e-mail já existente', async () => {
+		await request(app)
+			.post('/users')
+			.send({
+				name: 'Edmar',
+				email: 'edmar2@smn.com.br',
+				password: '123456',
+			});
+
+		const response = await request(app)
+			.post('/users')
+			.send({
+				name: 'Edmar',
+				email: 'edmar2@smn.com.br',
+				password: '123456',
+			});
+
+		expect(response.status).toBe(400);
+	});
+});
