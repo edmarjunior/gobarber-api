8adcbf0cd7967091bdfdbb9d51c9a96b5bfce3e2
Edmar Junior <edmarjunior17@gmail.com>
2020-03-19 20:39:41 -0300

    Criação do teste para validar a senha do usuario

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,6 +2,8 @@
 
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
+import User from '../../src/app/models/User';
 
 describe('User', () => {
 	beforeEach(async () => {
@@ -37,5 +39,16 @@ describe('User', () => {
 			});
 
 		expect(response.status).toBe(400);
+	});
+
+	it('Deve validar o password_hash', async () => {
+		const user = await User.create({
+			name: 'Edmar',
+			email: 'edmar2@smn.com.br',
+			password: '123456',
+		});
+
+		const compare = await user.checkPassword('123456');
+		expect(compare).toBe(true);
 	});
 });
