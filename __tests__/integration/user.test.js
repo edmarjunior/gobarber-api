57232945724e31cb387e0aff41d31e850f3d2275
Edmar Junior <edmarjunior17@gmail.com>
2020-03-19 21:33:53 -0300

    Implementação de factory e faker de user (nos tests)

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -3,7 +3,7 @@
 import request from 'supertest';
 import app from '../../src/app';
 import truncate from '../util/truncate';
-import User from '../../src/app/models/User';
+import factory from '../factories';
 
 describe('User', () => {
 	beforeEach(async () => {
@@ -11,40 +11,31 @@ describe('User', () => {
 	});
 
 	it('Deve ser possível realizar o cadastro', async () => {
+		const user = await factory.attrs('User');
+
 		const response = await request(app)
 			.post('/users')
-			.send({
-				name: 'Edmar',
-				email: 'edmar2@smn.com.br',
-				password: '123456',
-			});
+			.send(user);
+
 		expect(response.body).toHaveProperty('id');
 	});
 
 	it('Não deve permitir cadastrar usuário com e-mail já existente', async () => {
+		const user = await factory.attrs('User');
+
 		await request(app)
 			.post('/users')
-			.send({
-				name: 'Edmar',
-				email: 'edmar2@smn.com.br',
-				password: '123456',
-			});
+			.send(user);
 
 		const response = await request(app)
 			.post('/users')
-			.send({
-				name: 'Edmar',
-				email: 'edmar2@smn.com.br',
-				password: '123456',
-			});
+			.send(user);
 
 		expect(response.status).toBe(400);
 	});
 
 	it('Deve validar o password_hash', async () => {
-		const user = await User.create({
-			name: 'Edmar',
-			email: 'edmar2@smn.com.br',
+		const user = await factory.create('User', {
 			password: '123456',
 		});
 
