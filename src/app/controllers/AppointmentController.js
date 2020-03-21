77453700a521502ea8d60d1b61a46d2178506300
Edmar Junior <edmarjunior17@gmail.com>
2020-03-20 21:57:19 -0300

    Criação e utilização do CreateAppointmentService

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,12 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
-import pt from 'date-fns/locale/pt';
+import { isBefore, subHours } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-import Notification from '../schemas/Notification';
 import CancellationMail from '../jobs/CancellationMail';
 import Queue from '../../lib/Queue';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 class AppointmentController {
 	async index(req, res) {
@@ -52,67 +51,10 @@ class AppointmentController {
 
 		const { provider_id, date } = req.body;
 
-		/**
-		 * checando se o provider_id é um provedor
-		 */
-
-		const provider = await User.findOne({
-			where: {
-				id: provider_id,
-				provider: true,
-			},
-		});
-
-		if (!provider) {
-			return res.status(401).json({ error: 'Provedor não encontrado' });
-		}
-
-		const hourStart = startOfHour(parseISO(date));
-
-		/**
-		 * Checando datas passadas
-		 */
-		if (isBefore(hourStart, new Date())) {
-			return res.status(400).json({ error: 'Não é permitido data passada' });
-		}
-
-		/**
-		 * Checando disponibilidade do provedor
-		 */
-
-		const scheduledAppointment = await Appointment.findOne({
-			where: {
-				provider_id,
-				canceled_at: null,
-				date: hourStart,
-			},
-		});
-
-		if (scheduledAppointment) {
-			return res.status(400).json({ error: 'Horário não disponivel' });
-		}
-
-		const appointment = await Appointment.create({
-			user_id: req.userId,
+		const appointment = await CreateAppointmentService.run({
 			provider_id,
 			date,
-		});
-
-		/**
-		 * Notificando o provedor de serviço
-		 */
-
-		const user = await User.findByPk(req.userId);
-
-		const formattedDate = format(
-			hourStart,
-			"'dia' dd 'de' MMMM', às' H:mm'h'",
-			{ locale: pt }
-		);
-
-		await Notification.create({
-			content: `Novo agendamento de ${user.name} para ${formattedDate}`,
-			user: provider_id,
+			user_id: req.userId,
 		});
 
 		return res.json(appointment);
---
+++ src/app/services/CreateAppointmentService.js
@@ -0,0 +1,76 @@
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
+
+class CreateAppointmentService {
+	async run({ provider_id, date, user_id }) {
+		/**
+		 * checando se o provider_id é um provedor
+		 */
+
+		const provider = await User.findOne({
+			where: {
+				id: provider_id,
+				provider: true,
+			},
+		});
+
+		if (!provider) {
+			throw new Error('Provedor não encontrado');
+		}
+
+		const hourStart = startOfHour(parseISO(date));
+
+		/**
+		 * Checando datas passadas
+		 */
+		if (isBefore(hourStart, new Date())) {
+			throw new Error('Não é permitido data passada');
+		}
+
+		/**
+		 * Checando disponibilidade do provedor
+		 */
+
+		const scheduledAppointment = await Appointment.findOne({
+			where: {
+				provider_id,
+				canceled_at: null,
+				date: hourStart,
+			},
+		});
+
+		if (scheduledAppointment) {
+			throw new Error('Horário não disponivel');
+		}
+
+		const appointment = await Appointment.create({
+			user_id,
+			provider_id,
+			date,
+		});
+
+		/**
+		 * Notificando o provedor de serviço
+		 */
+
+		const formattedDate = format(
+			hourStart,
+			"'dia' dd 'de' MMMM', às' H:mm'h'",
+			{ locale: pt }
+		);
+
+		const user = await User.findByPk(user_id);
+
+		await Notification.create({
+			content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+			user: provider_id,
+		});
+
+		return appointment;
+	}
+}
+
+export default new CreateAppointmentService();
