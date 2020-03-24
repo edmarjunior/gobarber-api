073e7dfa8390b084f5a9486eaef28032b70676db
Edmar Junior <edmarjunior17@gmail.com>
2020-03-24 09:23:38 -0300

    Comentando para retornar os erros para teste

--- src/app.js
+++ src/app.js
@@ -60,11 +60,11 @@ class App {
 
 	exceptionHandler() {
 		this.server.use(async (err, req, res, next) => {
-			if (process.env.NODE_ENV === 'development') {
-				const errors = await new Youch(err, req).toJSON();
-				return res.status(500).json(errors);
-			}
-			return res.status(500).json('Internal server error');
+			//	if (process.env.NODE_ENV === 'development') {
+			const errors = await new Youch(err, req).toJSON();
+			return res.status(500).json(errors);
+			// }
+			// return res.status(500).json('Internal server error');
 		});
 	}
 }
